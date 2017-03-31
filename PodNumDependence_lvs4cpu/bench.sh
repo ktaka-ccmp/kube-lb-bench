b8ca9b9ff34746e380fb34cbbd93bce5f3177618
Kimitoshi Takahshi <ktaka@ccmp.jp>
2017-03-31 16:26:55 +0000

    Initial commit

---
+++ PodNumDependence_lvs4cpu/bench.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+wait=20
+
+con=800
+thr=40
+dur=60s
+
+bench(){
+	echo wrk -c$con -t$thr -d$dur $url 
+	echo $repl >> $file
+	echo wrk -c$con -t$thr -d$dur $url >> $file
+	echo -n "Start time: " >> $file
+	date +%s >> $file
+	wrk -c$con -t$thr -d$dur $url >> $file
+	echo -n "End time: " >> $file
+	date +%s >> $file
+	echo >> $file
+}
+
+set_sg(){
+url=http://172.16.121.2/coffee
+file=single.log
+}
+
+set_px(){
+url=http://10.254.0.10/coffee
+file=proxy.log
+}
+
+set_ig(){
+url=http://172.16.14.2/coffee
+file=ingress.log
+}
+
+set_lv(){
+url=http://10.0.255.10:8888/coffee
+file=lvs.log
+}
+
+kubectl scale rc/coffee-rc --replicas=1 -s 10.0.0.100:8080
+set_sg ; bench 
+
+#for repl in {1..10} ; do 
+for repl in 1 $(seq 2 2 40) ; do 
+
+echo start measurement for $repl
+kubectl scale rc/coffee-rc --replicas=$repl -s 10.0.0.100:8080
+sleep $wait
+ssh -i ~/.ssh/id_rsa_v200 kt-lvs010 sudo svc -t /etc/service/ipvs
+#kubectl get pod -o wide -s 10.0.0.100:8080
+
+set_px ; bench 
+set_ig ; bench 
+set_lv ; bench 
+echo end measurement for $repl
+echo 
+
+done
+
+
