98f08714be480596d2f15bf050cacbad9ad68c09
Kimitoshi Takahshi <ktaka@ccmp.jp>
2017-04-05 15:16:25 +0000

    Fix bad CSV

--- Repeatability_lvs2-16cpu.3/anal.sh
+++ Repeatability_lvs2-16cpu.3/anal.sh
@@ -5,5 +5,5 @@ file=lvs_cpu${1}.log
 egrep "podnum|Requ" $file \
 |awk '/^podnum/ { printf("%s,", $0); next } 1' \
 |egrep -v "podnum=.*,podnum" \
-| sed -e 's/podnum=//g' -e 's/,Requests\/sec:  / /g'
+| sed -e 's/podnum=//g' -e 's/Requests\/sec:  //g'
 
