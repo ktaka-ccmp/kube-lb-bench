f46a59503aac8d968fd9bcce7d949d5f0b3d86a0
Kimitoshi Takahshi <ktaka@ccmp.jp>
2017-04-06 02:45:56 +0900

    wip

--- Repeatability_lvs2-16cpu.3/anal.sh
+++ Repeatability_lvs2-16cpu.3/anal.sh
@@ -1,9 +1,23 @@
 #/bin/bash
 
+anal(){
 file=lvs_cpu${1}.log
 
+echo \"# of pods\",\"Req/sec\"
 egrep "podnum|Requ" $file \
 |awk '/^podnum/ { printf("%s,", $0); next } 1' \
 |egrep -v "podnum=.*,podnum" \
-| sed -e 's/podnum=//g' -e 's/Requests\/sec:  //g'
+| sed -E -e 's/podnum=//g' -e 's/Requests\/sec:[[:space:]]+//g'
+}
+
+for i in 02 04 08 12 16 ; do 
+	anal $i > cpu$i.csv 
+done 
+
+for j in 1 10 20 30 40 ; do 
+	echo \"# of cpu on LB\",\"Req/sec\" > pod${j}.csv
+	for i in 02 04 08 12 16 ; do 
+		egrep "${j}," cpu${i}.csv|sed -e 's/'${j}',/'$i',/g' 
+	done >> pod${j}.csv 
+done
 
