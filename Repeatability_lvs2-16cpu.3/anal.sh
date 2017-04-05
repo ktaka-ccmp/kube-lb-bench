b586846ab84b1db446ae5860ae0ae511a48ee8ab
Kimitoshi Takahshi <ktaka@ccmp.jp>
2017-04-05 15:13:04 +0000

    Add: csv result

---
+++ Repeatability_lvs2-16cpu.3/anal.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+file=lvs_cpu${1}.log
+
+egrep "podnum|Requ" $file \
+|awk '/^podnum/ { printf("%s,", $0); next } 1' \
+|egrep -v "podnum=.*,podnum" \
+| sed -e 's/podnum=//g' -e 's/,Requests\/sec:  / /g'
+
