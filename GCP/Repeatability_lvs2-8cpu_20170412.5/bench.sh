92e139d99583de7c87d6fecbc97c29cdc640afeb
Kimitoshi Takahshi <ktaka@ccmp.jp>
2017-04-12 10:37:29 +0000

    Repeatability for 8cpu lvs x 8 times with same instance, kt-lvs002

---
+++ GCP/Repeatability_lvs2-8cpu_20170412.5/bench.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+wait=30
+
+con=800
+thr=40
+dur=60s
+
+bench(){
+	echo wrk -c$con -t$thr -d$dur $url 
+	echo podnum=$repl >> $file
+	echo -n "Start time: " >> $file
+	date +%s >> $file
+	echo wrk -c$con -t$thr -d$dur $url >> $file
+	wrk -c$con -t$thr -d$dur $url >> $file
+	echo -n "End time: " >> $file
+	date +%s >> $file
+	echo >> $file
+}
+
+set_lv02(){
+url=http://10.0.255.2:8888/
+file=cpu08_lv02.log
+}
+
+bench_set(){
+for repl in 1 $(seq 2 2 20) 25 30 ; do 
+
+kubectl scale rc/coffee-rc --replicas=0 -s 10.0.0.100:8080
+
+echo start measurement for $repl
+
+kubectl scale rc/coffee-rc --replicas=$repl -s 10.0.0.100:8080
+sleep $wait
+
+ipvs_restart(){
+for lv in 002 ; do 
+	ssh -i ~/.ssh/id_rsa_v200 kt-lvs$lv sudo svc -t /etc/service/ipvs 
+done
+sleep 10 ; ipvs_check
+}
+
+ipvs_check(){
+for lv in 002 ; do 
+        workers=$(ssh -i ~/.ssh/id_rsa_v200 kt-lvs$lv 'sudo ipvsadm -L -n|egrep :80 |wc -l')
+        if [ $workers != $repl ]; then
+                ipvs_restart
+        fi
+done
+}
+
+ipvs_restart
+sleep $wait
+
+for try in 1 2 3 4 5 6 7 8 ; do 
+set_lv02 
+file=cpu08_lv02_${try}.log
+bench
+done
+
+echo end measurement for $repl
+echo 
+
+done
+}
+
+for i in {1..1}; do
+bench_set
+done
+
