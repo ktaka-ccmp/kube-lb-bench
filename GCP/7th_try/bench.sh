1f5cf887cf7c269f342b64ea58508b90901c1e8e
Kimitoshi Takahshi <ktaka@ccmp.jp>
2017-09-28 08:20:05 +0000

    7th_try: GCP repeatability test.

---
+++ GCP/7th_try/bench.sh
@@ -0,0 +1,90 @@
+#!/bin/bash 
+
+wait=3
+
+con=800
+thr=40
+dur=30s
+
+repl=40
+
+sudo sh -c "echo 3 >  /proc/sys/net/ipv4/tcp_syn_retries"
+
+kbctl="kubectl -s 10.0.0.100:8080 "
+
+########## functions
+
+bench(){
+        chk_url
+
+        echo wrk -c$con -t$thr -d$dur $url 
+        echo podnum=$repl >> $file
+        echo -n "Start time: " >> $file
+        date +%s >> $file
+        echo wrk -c$con -t$thr -d$dur $url >> $file
+        while ! wrk -c$con -t$thr -d$dur $url >> $file ; do
+                chk_url
+        done
+        echo -n "End time: " >> $file
+        date +%s >> $file
+        echo >> $file
+}
+
+chk_url(){
+        sudo ip route flush cache
+        sleep $wait
+        while ! curl $url ; do
+                sudo ip route flush cache
+                sleep $wait
+                echo Retrying ....
+        done
+}
+
+pod_check(){
+        sleep $wait
+        running=$($kbctl get po |egrep tea |egrep Running | wc -l)
+        not_running=$($kbctl get po |egrep tea |egrep -v Running | wc -l)
+        if [ $running != $repl ] || [ $not_running != 0 ] ; then
+                pod_check
+        fi
+}
+
+######### Measurement
+
+bench_set(){
+routes=$($kbctl get pod -o wide |egrep ipvs-controller |awk '{print $7"+"$6}'|sort)
+
+for route in $routes ; do 
+
+	$kbctl scale deploy/tea-rc --replicas=$repl
+	pod_check
+
+	lvsip=${route#*+}
+	node=${route%+*}
+	nodeip=$(getent hosts $node |awk '{ print $1 }')
+	url=http://$lvsip:8888/
+
+	file=ipvs_on_$node.log
+
+	if [ "$(gcloud compute routes describe $node  |yq -r .nextHopIp)" != "$nodeip" ] ; then
+		gcloud compute routes delete $node -q 
+		gcloud compute routes create $node --network ktaka-net \
+		--destination-range=$lvsip --next-hop-address $nodeip --priority 100 
+	fi
+
+	bench
+
+done 
+}
+
+RSS=0;RPS=1;RFS=0
+
+for i in {1..100} ; do
+	echo -n "n=" $i "; "
+	date
+	dir=rss_rps_rfs_${RSS}_${RPS}_${RFS}
+	mkdir -p $dir && \
+	(cd $dir ; bench_set)
+done
+
+
