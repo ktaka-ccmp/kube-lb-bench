99a717de0f8e4230c0c7eec344304376acc7950a
Kimitoshi Takahashi <ktaka@ccmp.jp>
2018-01-11 17:06:46 +0000

    9th_try 8cpu nodes experiments

--- GCP/9th_try/chk.sh
+++ GCP/9th_try/chk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-for file in */*3*.log ; do echo -n $file ': '; egrep Request $file  |awk '{print $2}'|tr '\n' ' ';echo; done
+for file in */*08*.log ; do echo -n $file ': '; egrep Request $file  |awk '{print $2}'|tr '\n' ' ';echo; done
 
