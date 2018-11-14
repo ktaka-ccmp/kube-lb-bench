491d81349bc2351349ff604a672a8b4b00723c09
Kimitoshi Takahshi <ktaka@ccmp.jp>
2018-11-14 23:08:08 +0900

    LVS-TUN @ 10Gbps

--- KuruwaNew/Try02_cubic_lvstun/chk.sh
+++ KuruwaNew/Try02_cubic_lvstun/chk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-for file in */*.log ; do echo -n ${file##*/} ': '; egrep Request $file  |awk '{print $2}'|tr '\n' ' ';echo; done
+for file in rss_rps_rfs_xps_1*/*.log ; do echo -n ${file##*/} ': '; egrep Request $file  |awk '{print $2}'|tr '\n' ' ';echo; done
 
