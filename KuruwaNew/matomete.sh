67565868b201b47cadf5f61705e9329112154b39
Kimitoshi Takahshi <ktaka@ccmp.jp>
2018-11-17 10:11:00 +0900

    LVS-TUN retry

--- KuruwaNew/matomete.sh
+++ KuruwaNew/matomete.sh
@@ -6,7 +6,8 @@
 #DIRS="Try02_cubic Try02_bbr Try01_bbr Try03_cubic Try03_bbr"
 #DIRS=Try01_cubic
 #DIRS="Try02_cubic_lvstun Try01_cubic_lvstun"
-DIRS="Try02_cubic_iptablesdnat Try01_cubic_lvstun"
+#DIRS="Try02_cubic_iptablesdnat Try01_cubic_lvstun"
+DIRS="Try01_cubic_lvstun"
 log=exec.log
 
 for dir in $DIRS ; do 
