7f02e2d505ed6d27042ac6f7a945dcbcb4b6d5a6
Kimitoshi Takahshi <ktaka@ccmp.jp>
2019-06-25 13:01:37 +0900

    CPU idle in 10G #3

--- KuruwaNew/Try01_cubic_10g/cpuidle.sh
+++ KuruwaNew/Try01_cubic_10g/cpuidle.sh
@@ -113,7 +113,7 @@ scp $hst:~/dstat_$repl.csv ./
 
 bench_set(){
 
-for try in {2..2} ; do
+for try in {3..3} ; do
 for ipvs in {1..1}; do
 for repl in 1 $(seq 2 2 20) $(seq 25 5 70) ; do
 #for repl in 1 10 50 100 ; do 
