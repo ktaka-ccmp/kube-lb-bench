f82f98ca1311526d4f27c411abd1dd2b7ddb3a6d
Kimitoshi Takahshi <ktaka@ccmp.jp>
2019-06-26 11:21:38 +0900

    xlb data #1

---
+++ KuruwaNew/Try01_cubic_10g_xdp/anal_test2.sh
@@ -0,0 +1,54 @@
+#/bin/bash
+
+anal(){
+
+echo \"# of pods\",\"Req/sec\"
+egrep "podnum|Requ" $1 \
+|awk '/^podnum/ { printf("%s,", $0); next } 1' \
+|egrep -v "podnum=.*,podnum" \
+| sed -E -e 's/podnum=//g' -e 's/Requests\/sec:[[:space:]]+//g'
+}
+
+anal2(){
+
+egrep "podnum|time:|Requ" $1 \
+|awk '/^podnum|^Start|^Requ/ { printf("%s ", $0); next } 1' \
+|sed -E -e 's/podnum=//g' -e 's/Start time: //g' -e 's/End time: //g' -e 's/Requests\/sec:[[:space:]]+//g' \
+|awk '{$2+=1;$4-=1;print}'
+}
+
+
+process(){
+        while read pod t1 rps t2 ; do
+
+               echo "$pod $rps $t1 $t2"
+                echo -n "$pod,$rps,"
+                awk "/$t1/,/$t2/" $file.dstat_$pod.csv \
+                | awk -F, '{idle+=$4} {siq+=$7} {eth0_rx+=$8} {eth0_tx+=$9} {d0_rx+=$8} {d0_tx+=$9} END \
+                {print idle,siq,eth0_rx,eth0_tx,d0_rx,d0_tx,NR}' OFS=,  
+#                {print idle/NR,siq/NR,eth0_rx/NR,eth0_tx/NR,d0_rx/NR,d0_tx/NR}' OFS=,  
+        done 
+#        done >> ${file%.log}.csv
+}
+
+anal_outer(){
+for file in ${prefix}*.log ; do 
+	echo "anal2 $file > ${file%.log}.csv "
+#	anal $file > ${file%.log}.csv
+
+	echo \"# of pods\",\"Req/sec\",\"cpu idle\",\"siq\",\"eth0-rx\,\"eth0-tx\,\"docker0-rx\,\"docker0-tx\" > ${file%.log}.csv
+ 	anal2 $file | process
+
+done 
+
+}
+
+for dir in cpuidle_rss_* ; do 
+echo $dir 
+(cd $dir 
+prefix=xlb ; anal_outer 
+#prefix=ipvstun ; anal_outer 
+#prefix=iptd ; anal_outer 
+)
+done
+
---
+++ KuruwaNew/Try01_cubic_10g_xdp/cpuidle.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+wait=1
+
+con=2000
+thr=100
+dur=30s
+#dur=15s
+#dur=3s
+
+ulimit -n 65536
+
+echo 3 >  /proc/sys/net/ipv4/tcp_syn_retries
+
+kbctl="kubectl -s 192.168.0.102:8080 "
+
+bench(){
+	dstat_begin
+
+	chk_url
+
+	echo wrk -c$con -t$thr -d$dur $url 
+	echo podnum=$repl >> $file
+	echo -n "Start time: " >> $file
+	date +%s >> $file
+	echo wrk -c$con -t$thr -d$dur $url >> $file
+	while ! wrk -c$con -t$thr -d$dur $url >> $file ; do
+		chk_url
+	done
+	echo -n "End time: " >> $file
+	date +%s >> $file
+	echo >> $file
+
+	dstat_end
+}
+
+chk_url(){
+	ip route flush cache
+	sleep $wait
+	while ! curl $url ; do
+		ip route flush cache
+		sleep $wait
+		echo Retrying ....
+	done
+}
+
+set_xlb(){
+url=http://10.1.1.2/
+file=xlb_$num.log
+}
+
+
+pod_check(){
+echo -n "pod_check : "
+	sleep $wait
+	running=$($kbctl get po |egrep tea |egrep Running | wc -l)
+	not_running=$($kbctl get po |egrep tea |egrep -v Running | wc -l)
+	echo "$running , $not_running"
+	if [ $running != $repl ] || [ $not_running != 0 ] ; then
+		pod_check
+	fi
+}
+
+
+dstat_begin(){
+script=./cpuidle.sh
+dur2=${dur%?}
+
+lvs_hosts=$($kbctl get node --show-labels|grep lvs=yes|cut -f 1 -d " ")
+for hst in $lvs_hosts ; do
+cat <<EOF | ssh $hst "cat - > $script "
+#!/bin/bash
+
+rm -f dstat_$repl.csv
+dstat -Tcn -N eth0,docker0,veth31e08a5 --output dstat_$repl.csv -i 1 $((dur2*2+5)) > /dev/null 2>&1
+EOF
+
+ssh $hst "chmod +x $script  &&  $script" &                                                
+done
+}
+
+dstat_end(){
+	wait
+	scp $hst:~/dstat_$repl.csv ./$file.dstat_$repl.csv
+}
+
+bench_set(){
+
+for try in {1..5} ; do
+for ipvs in {1..1}; do
+for repl in 1 $(seq 2 2 20) $(seq 25 5 60) ; do
+
+echo start measurement for $repl
+$kbctl scale deploy/tea-rc --replicas=1
+$kbctl scale deploy/tea-rc --replicas=$repl
+
+sleep 3
+$kbctl scale deploy/tea-rc --replicas=$repl
+
+pod_check
+sleep 3
+
+
+num=${ipvs}_$try
+
+set_xlb ; bench
+
+echo end measurement for try= $try, ipvs= $ipvs , repl= $repl
+echo 
+done
+done
+done
+}
+
+bench_set_set(){
+	echo "Measurement for RSS=$RSS;RPS=$RPS;RFS=$RFS;XPS=$XPS "
+	echo "Start time: $(date)"
+	start=$(date +%s)
+
+	lvs_hosts=$($kbctl get node --show-labels|grep lvs=yes|cut -f 1 -d " ")
+	for hst in $lvs_hosts ; do echo "RSS=$RSS;RPS=$RPS;RFS=$RFS;XPS=$XPS" | \
+	ssh $hst  "cat - > /etc/service/irq_set/env" ; done
+
+	sleep 6
+
+	dir=cpuidle_rss_rps_rfs_xps_${RSS}_${RPS}_${RFS}_${XPS}
+	mkdir -p $dir && \
+	cat /dev/null > $dir/irq_chk.status && \
+	for hst in $lvs_hosts ; do echo echo $hst >> $dir/irq_chk.status ; ssh $hst ./irq_chk.sh >> $dir/irq_chk.status ; done && \
+	(cd $dir ; bench_set)
+
+	echo -n "Elapsed time: "
+	date -d@$(($(date +%s) - $start))  -u +%H:%M:%S
+	echo
+}
+
+RSS=0;RPS=0;RFS=0;XPS=0 ; bench_set_set
+
